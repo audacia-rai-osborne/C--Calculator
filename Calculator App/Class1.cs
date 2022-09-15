df0a2f88cf676386b1f2bdf40b3e6cd0e48d9080
Rai Osborne <rai.osborne@audacia.co.uk>
2022-09-15 15:23:27 +0100

    feat: created calculator with classes

---
+++ Calculator App/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Arithmetic
+{
+    public int AddAnswer(int firstNumber, int secondNumber)
+    {
+        return (firstNumber + secondNumber);
+    }
+
+    public int SubtractAnswer(int firstNumber, int secondNumber)
+    {
+        return (firstNumber - secondNumber);
+    }
+
+    public int MultiplyAnswer(int firstNumber, int secondNumber)
+    {
+        return (firstNumber * secondNumber);
+    }
+
+    public int ModulusAnswer(int firstNumber, int secondNumber)
+    {
+        return (firstNumber % secondNumber);
+    }
+
+    public int DivideAnswer(int firstNumber, int secondNumber)
+    {
+        return (firstNumber / secondNumber);
+    }
+
+}
---
+++ Calculator App/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var math = new Arithmetic();
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+var operationType = Console.ReadLine();
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+var operationTypeTwo = Console.ReadLine();
+var thirdNumber = Convert.ToInt32(Console.ReadLine());
+int firstResult = 0;
+
+if (operationType == "+") {
+    firstResult = math.AddAnswer(firstNumber, secondNumber);
+}else if (operationType == "-")
+{
+    firstResult = math.SubtractAnswer(firstNumber, secondNumber);
+}
+else if (operationType == "*")
+{
+    firstResult = math.MultiplyAnswer(firstNumber, secondNumber);
+}
+else if (operationType == "/")
+{
+    firstResult = math.DivideAnswer(firstNumber, secondNumber);
+}
+else if (operationType == "%")
+{
+    firstResult = math.ModulusAnswer(firstNumber, secondNumber);
+};
+
+if(operationTypeTwo == "")
+{
+    Console.WriteLine(firstResult);
+}else if (operationTypeTwo == "+")
+{
+    Console.WriteLine(math.AddAnswer(firstResult, secondNumber));
+}
+else if (operationTypeTwo == "-")
+{
+    Console.WriteLine(math.SubtractAnswer(firstResult, secondNumber));
+}
+else if (operationTypeTwo == "*")
+{
+    Console.WriteLine(math.MultiplyAnswer(firstResult, secondNumber));
+}
+else if (operationTypeTwo == "/")
+{
+    Console.WriteLine(math.DivideAnswer(firstResult, secondNumber));
+}
+else if (operationTypeTwo == "%")
+{
+    Console.WriteLine(math.ModulusAnswer(firstResult, secondNumber));
+};
+
+
+
+
