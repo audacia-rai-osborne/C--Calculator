7e56229d053a4b47daec32554ce7c4e1316a0b8d
Rai Osborne <rai.osborne@audacia.co.uk>
2022-09-22 16:22:09 +0100

    feat: improved methods, added error handling

--- Calculator App/Program.cs
+++ Calculator App/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var add = new Addition();
-var sub = new Subtraction();
-var multi = new Multiply();
-var divide = new Divide();
-var mod = new Modulus();
+//var add = new Addition();
+//var sub = new Subtraction();
+//var multi = new Multiply();
+//var divide = new Divide();
+//var mod = new Modulus();
 
 List<Calculator_App.iArithmetic> _all_operators;
 
@@ -15,18 +15,11 @@ _all_operators = new List<Calculator_App.iArithmetic>()
                 new Multiply(),
                 new Divide(),
                 new Modulus(),
-                new PowerOfAnswer()
-            };
-
-List<Calculator_App.iSingleDigit> _all_single_operators;
-
-_all_single_operators = new List<Calculator_App.iSingleDigit>()
-            {
+                new PowerOfAnswer(),
                 new SinAnswer(),
                 new LogAnswer(),
                 new SquareRootAnswer()
             };
-
 
 decimal firstNumber;
 decimal secondNumber;
@@ -35,6 +28,7 @@ int numberOfOperators;
 int numberOfZeros = 0;
 string? operatorType;
 string? operatorTypeTwo;
+string secondNumberTake;
 
 List<decimal> _numbers = new List<decimal>();
 List<decimal> calcNumbers = new List<decimal>();
@@ -55,8 +49,37 @@ if (numberOfOperators == 1)
         operatorType = Console.ReadLine();
         _operators.Add(operatorType);
         Console.WriteLine("What is the second number of this operation? If there is none, enter 0");
-        secondNumber = Convert.ToDecimal(Console.ReadLine());
-        _numbers.Add(secondNumber);
+        string secondNumberTest = Console.ReadLine();
+        // this is a custom throw error for is users do not input a second number, wiht secondNumber updated to secondNumberTake and conversion to int removed
+        //if (string.IsNullOrEmpty(secondNumberTake))
+        //{
+        //    throw new Calculator_App.NumberNotFound();
+        //}
+        //else
+        //{
+        //    secondNumber = Convert.ToInt32(secondNumberTake);
+        //    _numbers.Add(secondNumber);
+        //};
+        // this a try-catch for if users don't input a second number
+        try
+        {
+            // some code that might throw an exception
+            secondNumber = Convert.ToInt32(secondNumberTest);
+            _numbers.Add(secondNumber);
+        }
+        catch (Exception e)
+        {
+            // do something specific with NotSupportedException
+            Console.WriteLine("A number was not given");
+            throw;
+
+        }
+
+        // this makes code actually work is secondNumber is updated to secondNumberTake and conversion to int is removed
+        //if (!string.IsNullOrEmpty(secondNumberTake)){
+        //    secondNumber = Convert.ToInt32(secondNumberTake);
+        //    _numbers.Add(secondNumber);
+        //}
     }
 else if (numberOfOperators == 2)
 {
@@ -85,19 +108,19 @@ switch (numberOfOperators)
     case 1:
             if (_operators[0] == "sin")
             {
-                calcOperators.Add(_all_single_operators[0]);
+                calcOperators.Add(_all_operators[0]);
                 calcNumbers.Add(_numbers[0]);
                 ListSingleFinalCalculation(calcOperators, calcNumbers);   
             }
             else if (_operators[0] == "log")
             {
-                calcOperators.Add(_all_single_operators[1]);
+                calcOperators.Add(_all_operators[1]);
                 calcNumbers.Add(_numbers[0]);
                 ListSingleFinalCalculation(calcOperators, calcNumbers);
             }
             else if(_operators[0] == "sqrt")
             {
-                calcOperators.Add(_all_single_operators[2]);
+                calcOperators.Add(_all_operators[2]);
                 calcNumbers.Add(_numbers[0]);
                 ListSingleFinalCalculation(calcOperators, calcNumbers);
             }
@@ -205,7 +228,8 @@ switch (numberOfOperators)
                    calcNumbers.Add(_numbers[0]);
                    calcNumbers.Add(_numbers[1]);
                    calcNumbers.Add(_numbers[2]);
-                }
+                        ListSingleFinalCalculation(calcOperators, calcNumbers);
+                    }
                 else if (_operators[1] == "-")
                 {
             calcOperators.Add(_operators[0]);
@@ -213,6 +237,7 @@ switch (numberOfOperators)
             calcNumbers.Add(_numbers[0]);
             calcNumbers.Add(_numbers[1]);
             calcNumbers.Add(_numbers[2]);
+                        ListSingleFinalCalculation(calcOperators, calcNumbers);
                     }
                 else if (_operators[1] == "*")
                 {
@@ -376,17 +401,22 @@ void ListSingleFinalCalculation(List<dynamic> operation, List<decimal> numbers )
 {
     if (operation.Count > 1)
     {
-        decimal firstResult = operation[0].Do(numbers[0], numbers[1]);
-        decimal result = operation[1].Do(_numbers[3], firstResult);
+            Console.WriteLine("Hello");
+            decimal firstResult = operation[0].Do(_numbers[0], _numbers[1]);
+            decimal result = operation[1].Do(_numbers[3], firstResult);
         ScientificResult(result);    
     }
-    else
+    else if (operation.Count == 1 && numbers.Count == 1)
     {
 
         decimal result = operation[0].Do(_numbers[0]);
-
         ScientificResult(result);
-    }
+        }
+        else
+        {
+            decimal result = operation[0].Do(_numbers[0], _numbers[1]);
+            ScientificResult(result);
+        }
 
 }
 
@@ -410,12 +440,12 @@ void ScientificResult(decimal finalResult){
                 _outputResult.Add(splitFinalResult[1][numberOfZeros + 2]);
                 _outputResult.Add("x 10 ^-");
                 _outputResult.Add(numberOfZeros + 1);
-                Console.WriteLine("Your result is - " + string.Join("", _outputResult));
+                Console.WriteLine("Your result is: " + string.Join("", _outputResult));
             }
         }
         else
         {
-            Console.WriteLine("Your result is - " + stringFinalResult);
+            Console.WriteLine("Your result is: " + stringFinalResult);
         }
 
         
